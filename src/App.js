f523d692961462f83223b768969764b865671c63
Anuththara Rajakaruna <rkmpar0762@gmail.com>
2023-12-16 21:15:31 +0530

    updated_v2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import WeatherHome from "./Component/WeatherHome";
+import NavBar from "./Component/NavBar";
+
+const api = {
+  key: "7d4f151c7d97b3c9b10ce95280eac359",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full">
+      <div className="pl-2 pr-2 pt-2">
+        <NavBar />
+      </div>
+      {/* Search Box  */}
+
+      <div className="pt-3 pl-2 pr-2">
+        <WeatherHome />
+      </div>
     </div>
   );
 }
---
+++ src/Component/WeatherHome.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState } from "react";
+import iconSearch from "../Assets/Image/iconSearch.png";
+
+const api = {
+  key: "7d4f151c7d97b3c9b10ce95280eac359",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function WeatherHome() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  };
+
+  return (
+    <div class="h-[605px] w-full border-4 bg-slate-400 md:box-content bg-bg-weather_block rounded-3xl">
+      <div className="flex items-center justify-center pt-12">
+        <div className="w-3/6 h-10 bg-white rounded-3xl flex items-center ">
+          <div
+            onClick={searchPressed}
+            className="w-full h-full justify-between items-center flex"
+          >
+            <div className=" items-center justify-center w-full h-full">
+              <input
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Enter your location..."
+                required
+                className="ml-9 mt-2"
+              />
+            </div>
+
+            <div className="rounded-full h-5 w-5 flex mr-6">
+              <img src={iconSearch} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        {typeof weather.main !== "undefined" ? (
+          <div className="flex flex-col text-center items-center justify-center pt-4">
+            {/* Location  */}
+            <div className="max-w-lg text-6xl font-semibold leading-loose text-gray-900 dark:text-black -mb-5">
+              <p>{weather.name}</p>
+            </div>
+
+            <div className="flex flex-row justify-between items-center">
+              <div className="">
+                
+              </div>
+              <div className="text-4xl text-blue-500 font-bold">
+                {/* Temperature */}
+                <p>{weather.main.temp}°C</p>
+              </div>
+            </div>
+
+            <div>
+              {/* Condition*/}
+              <p className="text-4xl text-black-500 font-bold">{weather.weather[0].main}</p>
+              <p className="text-4xl text-black-500 font-bold">({weather.weather[0].description})</p>
+            </div>
+          </div>
+        ) : (
+          "Invalid Input"
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default WeatherHome;
